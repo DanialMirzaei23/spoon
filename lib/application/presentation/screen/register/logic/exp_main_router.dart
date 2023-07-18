c7458b08b25cabdefcdc11f30fd4ce62bc32e672
DanialMirzaei23 <danialmirzaei23@yahoo.com>
2023-07-18 19:32:26 +0330

    see you

---
+++ lib/application/presentation/screen/register/logic/exp_main_router.dart
@@ -0,0 +1,9 @@
+// todo => export main file router LOGIC
+
+
+//Bloc-onboarding
+export '../logic/bloc/bloc.dart';
+//Event-onboarding
+export '../logic/event/eve_value.dart';
+//State-onboarding
+export '../logic/state/ste_value.dart';
