3c2572f141d776820fecce22ea46d9f79ff6213c
Danial Mirzaei23 <danialmirzaei23@gmail.com>
2023-08-28 16:11:13 +0330

    end UI

--- lib/application/presentation/screen/home/features/favoriteScreen/favoriteScreen.dart
+++ lib/application/presentation/screen/home/features/favoriteScreen/favoriteScreen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:spoon/application/presentation/modules/extension/exp_main_router.dart';
+import 'package:spoon/application/presentation/modules/mixin/exp_main_router.dart';
+import 'package:spoon/application/presentation/modules/widget/exp_main_router.dart';
 
 import '../../../../../packages/package.dart';
 
@@ -7,8 +10,120 @@ class FavoriteScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Color> selectColors = [Colors.pinkAccent,Colors.blue,Colors.black,];
     return  Center(
-      child: 'danial234466666666666'.toText(context: context),
+      child: Column(
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: "15 items".toText(context: context,color: GenerateDataColors.grey_neutral.toHex,fontWeight: FontWeight.bold).toSpace(context: context,top: .01,bottom: .01),
+          ),
+          Align(
+            child: Container(
+// height: context.mediaQueryHeight(context),
+              width: context.mediaQueryWidth(context),
+              decoration: BoxDecoration(
+// color: Colors.red,
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              child: GridView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: 15,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    childAspectRatio: 10,
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 8,
+                    mainAxisExtent: context.mediaQueryWidth(context) * .8,
+                    mainAxisSpacing: 10),
+                itemBuilder: (context, index) => Container(
+// height: context.mediaQueryHeight(context) * .5,
+                  decoration: BoxDecoration(
+                      color: GenerateDataColors.white_neutral.toHex,
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Stack(
+                        children: [
+                          SvgGenerated(
+                            generate: Generate.image,
+                            router: GenerateDataImages.card,
+                            width: context.mediaQueryWidth(context),
+                            height: context.mediaQueryHeight(context) * .25,
+                          ),
+                          Align(
+                            alignment: Alignment.topRight,
+                            child: Container(
+                                decoration: BoxDecoration(
+                                    color: GenerateDataColors.white_neutral.toHex,
+                                    borderRadius: const BorderRadius.only(
+                                        bottomLeft: Radius.circular(15),
+                                        topRight: Radius.circular(15))),
+                                child: IconButton(
+                                  onPressed: () {},
+                                  // CupertinoIcons.heart_fill
+                                  icon: const Icon(CupertinoIcons.heart_fill,color: Colors.red,),
+                                )),
+                          )
+                        ],
+                      ),
+                      toSpaceVertical(context: context,size: .01),
+                      Align(
+                        alignment: Alignment.center,
+                        child: "Basic round neck oversize sweatshirt"
+                            .toText(context: context, fontWeight: FontWeight.w500),
+                      ),
+                      toSpaceVertical(context: context,size: .01),
+                      Align(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            "\$20".toText(
+                                context: context,
+                                fontSize: GenerateStyleFont.headline2,
+                                fontWeight: FontWeight.bold,
+                                color: GenerateDataColors.orange_primary.toHex),
+                            toSpaceLine(context: context,size: .1),
+                            SizedBox(
+                              height: 20,
+                              width: context.mediaQueryWidth(context)*.2,
+                              child: ListView.builder(
+                                  scrollDirection: Axis.horizontal,
+                                  itemBuilder: (context, index) {
+                                    return index == 3
+                                        ? Container(
+                                      alignment: Alignment.center,
+                                      width: 17,
+                                      height: 17,
+                                      decoration: BoxDecoration(
+                                          border: Border.all(color: GenerateDataColors.dark_neutral.toHex),
+                                          shape: BoxShape.circle),
+                                      child: Icon(CupertinoIcons.plus,size: 12,fill: .2,color: GenerateDataColors.dark_neutral.toHex),
+                                    ).toSpace(context: context,left: .005).generateButton(onTap: (){})
+                                        : Container(
+                                      width: 15,
+                                      height: 15,
+                                      decoration: BoxDecoration(
+                                          color: selectColors[index],
+                                          shape: BoxShape.circle),
+                                    ).toSpace(context: context,left: .005,right: .005);
+                                  },
+                                  itemCount: 4),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ).toSpace(context: context, right: .014, left: .014),
+                ),
+              ).toSpace(context: context,bottom: .0),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
+
--- lib/application/presentation/screen/home/features/setting/setting.dart
+++ lib/application/presentation/screen/home/features/setting/setting.dart
@@ -1,5 +1,6 @@
 import 'package:spoon/application/presentation/modules/extension/exp_main_router.dart';
-
+import 'package:spoon/application/presentation/modules/widget/exp_main_router.dart';
+import 'package:spoon/application/presentation/modules/widget/white_container.dart';
 import '../../../../../packages/package.dart';
 import '../../../../modules/mixin/exp_main_router.dart';
 
@@ -10,8 +11,35 @@ class Setting extends StatelessWidget {
   Widget build(BuildContext context) {
     return MediaScaffold.nonScaffold(
         context: context,
-        child: "danial".toText(context: context),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: [
+                toSpaceVertical(context: context,size: .015),
+                WhiteContainerGenerated(child: GenerateListTile(image: GenerateDataImages.language_setting, title: "Language", icon:GenerateDataImages.arrow_drawer)),
+                toSpaceVertical(context: context,size: .015),
+                WhiteContainerGenerated(child: GenerateListTile(image: GenerateDataImages.contact_setting, title: "Contact", icon:GenerateDataImages.arrow_drawer)),
+                toSpaceVertical(context: context,size: .015),
+                WhiteContainerGenerated(child: GenerateListTile(image: GenerateDataImages.info_setting, title: "Change password", icon:GenerateDataImages.arrow_drawer)),
+              ].toItem,
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: SizedBox(
+                width: context.mediaQueryWidth(context),
+                height: context.mediaQueryHeight(context)*.07,
+                child: GradientButton(onPressed: (){
+                  context.navigatorPush(onGenerated: onGenerateRouters.onStream);
+                }, title: "Back To Home"),
+              ),
+            ).toSpace(context: context,bottom: .02),
+          ],
+        ),
         titleAppBar: "Setting",
-        reActionIconAppBar: () {});
+        reActionIconAppBar: () {
+          context.navigatorPush(onGenerated: onGenerateRouters.onStream);
+        });
   }
 }
