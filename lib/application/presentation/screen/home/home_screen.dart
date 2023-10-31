6647fad7cf5cd060846f6f70bb1b7903aeedd734
imamal07 <abolizadeh.m@gmail.com>
2023-10-31 12:40:12 +0330

    add web platform to project

--- lib/application/presentation/screen/home/home_screen.dart
+++ lib/application/presentation/screen/home/home_screen.dart
@@ -11,51 +11,78 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return [
-        toSpaceVertical(context: context,size: .01),
-        SizedBox(
-          child: Row(
-            children: [
-            Expanded(flex: 1,child: SearchGenerate(titleField:context.toAppLocalization.search ),),// <------ No l10n file
-            toSpaceLine(context: context,size: .02),
-            Expanded(flex: 0,child:
-            Container(
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                color: GenerateDataColors.white_neutral.toHex,
-                borderRadius: BorderRadius.circular(10)
-              ),
-              child: SvgGenerated(generate: Generate.asset, router: GenerateDataImages.setting_group,
-                width:context.mediaQueryWidth(context)*.02 ,
-                height:context.mediaQueryHeight(context)*.02 ,
-              ).toSpace(context: context,right: .035,left: .035,bottom: .022,top: .022),
-            ).generateButton(onTap: (){}),),
-            ],
-          ),
+      toSpaceVertical(context: context, size: .01),
+      SizedBox(
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child:
+                  SearchGenerate(titleField: context.toAppLocalization.search),
+            ), // <------ No l10n file
+            toSpaceLine(context: context, size: .02),
+            Expanded(
+              flex: 0,
+              child: Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: GenerateDataColors.white_neutral.toHex,
+                    borderRadius: BorderRadius.circular(10)),
+                child: SvgGenerated(
+                  generate: Generate.asset,
+                  router: GenerateDataImages.setting_group,
+                  width: context.mediaQueryWidth(context) * .02,
+                  height: context.mediaQueryHeight(context) * .02,
+                ).toSpace(
+                    context: context,
+                    right: .035,
+                    left: .035,
+                    bottom: .022,
+                    top: .022),
+              ).generateButton(onTap: () {}),
+            ),
+          ],
         ),
-        toSpaceVertical(context: context,size: .02),
-        const SizedBox(
-          child: BoxHome(),
+      ),
+      toSpaceVertical(context: context, size: .02),
+      const SizedBox(
+        child: BoxHome(),
+      ),
+      toSpaceVertical(context: context, size: .015),
+      SizedBox(
+        child: Row(
+          children: [
+            Expanded(
+                flex: 5,
+                child: Row(
+                  children: [
+                    context.toAppLocalization.new_arrival.toText(
+                        context: context,
+                        fontSize: GenerateStyleFont.title2,
+                        fontWeight: FontWeight.bold),
+                    SvgGenerated(
+                      generate: Generate.asset,
+                      router: GenerateDataImages.fire_icon,
+                      width: context.mediaQueryWidth(context) * .03,
+                      height: context.mediaQueryHeight(context) * .03,
+                    ).toSpace(
+                      context: context,
+                      left: .02,
+                    )
+                  ],
+                )),
+            Expanded(
+              child: context.toAppLocalization.see_all.toText(
+                  context: context,
+                  fontSize: GenerateStyleFont.body4,
+                  fontWeight: FontWeight.w700,
+                  color: GenerateDataColors.orange_primary.toHex),
+            )
+          ],
         ),
-        toSpaceVertical(context: context,size: .015),
-        SizedBox(
-          child: Row(
-
-            children: [
-              Expanded(flex: 5, child: Row(
-                children: [
-                  context.toAppLocalization.new_arrival.toText(context: context,fontSize: GenerateStyleFont.title2,fontWeight: FontWeight.bold),
-                  SvgGenerated(generate: Generate.asset, router: GenerateDataImages.fire_icon,
-                    width:context.mediaQueryWidth(context)*.03 ,
-                    height:context.mediaQueryHeight(context)*.03 ,
-                  ).toSpace(context: context,left: .02,)
-                ],
-              )),
-              Expanded(child: context.toAppLocalization.see_all.toText(context: context,fontSize: GenerateStyleFont.body4,fontWeight: FontWeight.w700,color: GenerateDataColors.orange_primary.toHex),)
-            ],
-          ),
-        ),// <------ No l10n file
-        toSpaceVertical(context: context,size: .01),
-        const BoxArrival()
-      ].toItem;
+      ), // <------ No l10n file
+      toSpaceVertical(context: context, size: .01),
+      const BoxArrival()
+    ].toItem;
   }
 }
